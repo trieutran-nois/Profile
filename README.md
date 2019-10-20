733716274394fef18ccd0ca3c532a325b5504287
Chanh Pham <phamchanh6599@gmail.com>
2019-10-20 23:51:56 +0700

    Update file

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Install
+Run:
++ npm install
++ npm install node-sass
+
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './component/Header/Header';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope, faKey, faTimes, faBars, faShoppingCart, faSortDown, faCircle, faHeart, faTachometerAlt, faFileInvoiceDollar,
+	faLuggageCart, faAsterisk, faStore, faBuilding, faUtensils, faUsers, faPen
+} from '@fortawesome/free-solid-svg-icons';
+
+import './App.scss';
+import { openModal } from './actions';
+
+library.add(faEnvelope, faKey, faTimes, faBars, faShoppingCart, faSortDown,faCircle, faHeart,faTachometerAlt, faFileInvoiceDollar,
+	faLuggageCart, faAsterisk, faStore, faBuilding, faUtensils, faUsers, faPen
+	);
+
+function App(props) {
+	const {setting} = props;
+
+	return (
+		<div className="App">
+			<Header />
+			<div className={`${setting.isOpenModal ? 'openModal' : ''}`}> </div>
+		</div>
+	);
 }
 
-export default App;
+const mapStateToProps = state => {
+    return {
+        setting: state.setting,
+    }
+}
+
+
+export default connect(mapStateToProps, null)(App);
